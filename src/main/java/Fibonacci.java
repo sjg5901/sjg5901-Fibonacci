6aa6ed44752a667c5a27fb34a46de850ca61946d
Skyler Giannetti <sjg5901@psu.edu>
2023-08-14 03:41:54 +0000

    Fibonacci challenge complete

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -15,6 +15,12 @@ public class Fibonacci {
      * @return the nth number of fibonacci sequence.
      */
     public int fib(int n){
-        return 0;
+        
+        if (n == 0) return 0;
+        else if (n == 1 || n == 2) return 1;
+        else {
+            return fib(n-2) + fib(n-1);
+        }
+
     }
 }
